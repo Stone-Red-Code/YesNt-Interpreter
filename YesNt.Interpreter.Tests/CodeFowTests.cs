3c78755858126fd0bb472978fca984c522e0a86a
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-04-21 14:27:39 +0200

    Add basic tests

---
+++ YesNt.Interpreter.Tests/CodeFowTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+namespace YesNt.Interpreter.Tests;
+
+[TestClass]
+public class CodeFlowTests
+{
+    [TestMethod]
+    public void FunctionTest()
+    {
+        List<string> lines = new List<string>()
+        {
+            "cal yes",
+            "fnc yes",
+            "!<result = 1",
+            "ret",
+            ">result"
+        };
+        YesNtAssert.IsLastLineEqual(lines, "1");
+    }
+
+    [TestMethod]
+    public void LabelsTest()
+    {
+        List<string> lines = new List<string>()
+        {
+            "<result = 1",
+            "jmp yes",
+            "<result = 0",
+            "lbl yes",
+            ">result"
+        };
+        YesNtAssert.IsLastLineEqual(lines, "1");
+    }
+
+    [TestMethod]
+    public void CalculationsTest()
+    {
+        Assert.Inconclusive();
+        YesNtAssert.IsLineEqual("10 * 10 !calc", (20).ToString());
+    }
+}
---
+++ YesNt.Interpreter.Tests/YesNtAssert.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using YesNt.Interpreter.Runtime;
+
+namespace YesNt.Interpreter.Tests;
+
+internal static class YesNtAssert
+{
+    private static readonly YesNtInterpreter yesNtInterpreter = new YesNtInterpreter();
+
+    static YesNtAssert()
+    {
+        yesNtInterpreter.Initialize();
+    }
+
+    public static void IsLastLineEqual(List<string> lines, string expected, int timeout = 1000)
+    {
+        AutoResetEvent onDone = new AutoResetEvent(false);
+
+        DebugEventArgs debugEventArgs = new DebugEventArgs();
+        yesNtInterpreter.OnLineExecuted += (er) =>
+        {
+            debugEventArgs = er ?? debugEventArgs;
+
+            if (er is null)
+            {
+                onDone.Set();
+            }
+        };
+
+        yesNtInterpreter.Execute(lines, true);
+
+        _ = onDone.WaitOne(TimeSpan.FromSeconds(timeout));
+
+        Assert.AreEqual(expected, debugEventArgs.CurrentLine);
+    }
+
+    public static void IsLineEqual(string line, string expected, int timeout = 1000)
+    {
+        AutoResetEvent onDone = new AutoResetEvent(false);
+        List<string> lines = new List<string>()
+        {
+           line
+        };
+
+        DebugEventArgs debugEventArgs = new DebugEventArgs();
+        yesNtInterpreter.OnLineExecuted += (er) =>
+        {
+            debugEventArgs = er ?? debugEventArgs;
+            onDone.Set();
+        };
+
+        yesNtInterpreter.Execute(lines, true);
+
+        _ = onDone.WaitOne(TimeSpan.FromMilliseconds(timeout));
+
+        Assert.AreEqual(expected, debugEventArgs.CurrentLine);
+    }
+}
